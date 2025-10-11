4ec42676401528193a4a47095e8fafa56c3978f5
Bartłomiej Krawczyk <bartikraw@gmail.com>
2025-10-11 15:13:43 +0200

    Configure initial gradle configuration for the library

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+    groovy
+
+    id("maven-publish")
+}
+
+group = "io.github.bartlomiejkrawczyk"
+description = "optimization-kotlin-dsl"
+
+kotlin {
+    jvmToolchain(17)
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/bartlomiejkrawczyk/MATCH-OPTIMIZER")
+        credentials {
+            username = System.getenv("GITHUB_ACTOR") ?: project.extra.properties["GITHUB_ACTOR"].toString()
+            password = System.getenv("GITHUB_TOKEN") ?: project.extra.properties["GITHUB_TOKEN"].toString()
+        }
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+dependencies {
+    implementation("com.google.ortools:ortools-java:9.14.6206")
+
+    testImplementation("org.spockframework:spock-core:2.4-M6-groovy-4.0")
+}
+
+extensions.configure<PublishingExtension>("publishing") {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/bartlomiejkrawczyk/optimization-kotlin-dsl")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("GITHUB_ACTOR").toString()
+                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("GITHUB_TOKEN").toString()
+            }
+        }
+    }
+}

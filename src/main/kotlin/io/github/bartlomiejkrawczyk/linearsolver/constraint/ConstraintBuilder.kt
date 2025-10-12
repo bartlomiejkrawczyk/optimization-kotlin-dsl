5dcbe0a5f4394271b319335120c044e7f12fd459
Bartłomiej Krawczyk <bartikraw@gmail.com>
2025-10-12 18:51:16 +0200

    Configure boolean operations

--- src/main/kotlin/io/github/bartlomiejkrawczyk/linearsolver/constraint/ConstraintBuilder.kt
+++ src/main/kotlin/io/github/bartlomiejkrawczyk/linearsolver/constraint/ConstraintBuilder.kt
@@ -1,6 +1,7 @@
 package io.github.bartlomiejkrawczyk.linearsolver.constraint
 
 import io.github.bartlomiejkrawczyk.linearsolver.OptimizerDslMarker
+import io.github.bartlomiejkrawczyk.linearsolver.expression.BooleanVariable
 import io.github.bartlomiejkrawczyk.linearsolver.expression.Expression
 import io.github.bartlomiejkrawczyk.linearsolver.expression.LinearExpression
 import io.github.bartlomiejkrawczyk.linearsolver.model.OptimizerExtensions
@@ -89,5 +90,120 @@ public open class ConstraintBuilder(
             right = other,
             relationship = Relationship.GREATER_EQUALS,
         )
+    }
+
+    public fun not(x: BooleanVariable, y: BooleanVariable): Constraint {
+        return y eq 1 - x
+    }
+
+    public infix fun BooleanVariable.notEq(other: BooleanVariable): Constraint {
+        return other eq 1 - this@notEq
+    }
+
+    public fun and(
+        first: BooleanVariable,
+        second: BooleanVariable,
+    ): Constraint {
+        return 2.0 eq first + second
+    }
+
+    @JvmName("infixAnd")
+    public infix fun BooleanVariable.and(
+        other: BooleanVariable,
+    ): Constraint {
+        return 2.0 eq this@and + other
+    }
+
+    public fun or(
+        first: BooleanVariable,
+        second: BooleanVariable,
+    ): Constraint {
+        return 1.0 le first + second
+    }
+
+    @JvmName("infixOr")
+    public infix fun BooleanVariable.or(
+        other: BooleanVariable,
+    ): Constraint {
+        return 1.0 le this@or + other
+    }
+
+    public fun xor(
+        first: BooleanVariable,
+        second: BooleanVariable,
+    ): Constraint {
+        return first + second eq 1
+    }
+
+    @JvmName("infixXor")
+    public infix fun BooleanVariable.xor(
+        other: BooleanVariable,
+    ): Constraint {
+        return this@xor + other eq 1
+    }
+
+    @JvmName("allOfVararg")
+    public fun allOf(vararg variables: BooleanVariable): Constraint {
+        return variables.sum() eq variables.size
+    }
+
+    public fun Array<BooleanVariable>.allOf(): Constraint {
+        return this@allOf.sum() eq size
+    }
+
+    public fun Collection<BooleanVariable>.allOf(): Constraint {
+        return this@allOf.sum() eq size
+    }
+
+    @JvmName("noneOfVararg")
+    public fun noneOf(vararg variables: BooleanVariable): Constraint {
+        return variables.sum() eq 0
+    }
+
+    public fun Array<BooleanVariable>.noneOf(): Constraint {
+        return this@noneOf.sum() eq 0
+    }
+
+    public fun Iterable<BooleanVariable>.noneOf(): Constraint {
+        return this@noneOf.sum() eq 0
+    }
+
+    @JvmName("nOfVararg")
+    public fun nOf(n: Int, vararg variables: BooleanVariable): Constraint {
+        return variables.sum() eq n
+    }
+
+    public fun Array<BooleanVariable>.nOf(n: Int): Constraint {
+        return this@nOf.sum() eq n
+    }
+
+    public fun Iterable<BooleanVariable>.nOf(n: Int): Constraint {
+        return this@nOf.sum() eq n
+    }
+
+    @JvmName("atLeastVararg")
+    public fun atLeast(n: Int, vararg variables: BooleanVariable): Constraint {
+        return variables.sum() ge n
+    }
+
+    public fun Array<BooleanVariable>.atLeast(n: Int): Constraint {
+        return this@atLeast.sum() ge n
+    }
+
+    public fun Iterable<BooleanVariable>.atLeast(n: Int): Constraint {
+        return this@atLeast.sum() ge n
+    }
+
+    @JvmName("atMostVararg")
+    public fun atMost(n: Int, vararg variables: BooleanVariable): Constraint {
+        return variables.sum() le n
+    }
+
+    public fun Array<BooleanVariable>.atMost(n: Int): Constraint {
+        return this@atMost.sum() le n
+    }
+
+    public fun Iterable<BooleanVariable>.atMost(n: Int): Constraint {
+        return this@atMost.sum() le n
     }
 }
